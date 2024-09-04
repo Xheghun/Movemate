d4780208fb724a92348c4a1d1a980db7025f829e
Xheghun <ysenruf@gmail.com>
2024-09-04 15:12:06 +0100

    calculate screen complete

--- app/src/main/java/com/xheghun/movemate/presentation/screens/CalculateScreen.kt
+++ app/src/main/java/com/xheghun/movemate/presentation/screens/CalculateScreen.kt
@@ -5,22 +5,35 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -38,10 +51,19 @@ import com.xheghun.movemate.presentation.ui.Spacer
 import com.xheghun.movemate.presentation.ui.theme.bluePrimary
 import com.xheghun.movemate.presentation.ui.theme.colorGreyLight
 import com.xheghun.movemate.presentation.ui.theme.colorGreyText
+import com.xheghun.movemate.presentation.ui.theme.colorOrange
 import com.xheghun.movemate.presentation.ui.theme.hintTextStyle
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun CalculateScreen(navController: NavController) {
+
+    val categoryOption =
+        listOf("Document", "Glass", "Liquid", "Food", "Electronics", "Product", "Others")
+    var selectedCategory by remember { mutableStateOf("") }
+
+    val scrollState = rememberScrollState()
+
     Column(Modifier.fillMaxWidth()) {
         //HEADER
         Box(
@@ -70,76 +92,122 @@ fun CalculateScreen(navController: NavController) {
 
         //BODY
         Column(Modifier.padding(12.dp)) {
-
-            SectionHeading("Packaging", "")
-
-            Spacer(8)
-            Card(
+            //FORM
+            Column(
                 Modifier
-                    .background(Color.White)
-                    .padding(12.dp)
+                    .weight(1f)
+                    .verticalScroll(scrollState)
             ) {
-                IconTextField(
-                    value = "",
-                    onValueChange = {},
-                    hintText = "Sender Location",
-                    icon = Icons.Default.KeyboardArrowDown
-                )
-                IconTextField(
-                    value = "",
-                    onValueChange = {},
-                    hintText = "Receiver Location",
-                    icon = Icons.Default.KeyboardArrowDown
-                )
-                IconTextField(
-                    value = "",
-                    onValueChange = {},
-                    hintText = "Approx Weight",
-                    icon = Icons.Default.KeyboardArrowDown
-                )
-            }
-            Spacer(12)
 
-            SectionHeading("Packaging", "What are you sending?")
+                SectionHeading("Packaging", "")
 
-            Spacer(12)
-
-            Card(
-                shape = RoundedCornerShape(6.dp)
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
+                Spacer(8)
+                Card(
+                    Modifier
                         .background(Color.White)
-                        .padding(10.dp)
+                        .padding(12.dp)
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.open_package),
-                        contentDescription = "",
-                        modifier = Modifier.size(30.dp)
+                    IconTextField(
+                        value = "",
+                        onValueChange = {},
+                        hintText = "Sender Location",
+                        icon = Icons.Default.KeyboardArrowDown
                     )
-
-                    Box(
-                        modifier = Modifier
-                            .padding(horizontal = 10.dp)
-                            .height(18.dp)
-                            .width(1.dp)
-                            .background(colorGreyText)
+                    IconTextField(
+                        value = "",
+                        onValueChange = {},
+                        hintText = "Receiver Location",
+                        icon = Icons.Default.KeyboardArrowDown
                     )
-
-                    Text(text = "Box", fontWeight = FontWeight.Bold, modifier = Modifier.weight(1f))
-                    Icon(
-                        Icons.Default.KeyboardArrowDown,
-                        contentDescription = "",
-                        tint = colorGreyText
+                    IconTextField(
+                        value = "",
+                        onValueChange = {},
+                        hintText = "Approx Weight",
+                        icon = Icons.Default.KeyboardArrowDown
                     )
                 }
+                Spacer(12)
+
+                SectionHeading("Packaging", "What are you sending?")
+
+                Spacer(12)
+
+                Card(
+                    shape = RoundedCornerShape(6.dp)
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .background(Color.White)
+                            .padding(10.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.open_package),
+                            contentDescription = "",
+                            modifier = Modifier.size(30.dp)
+                        )
+
+                        Box(
+                            modifier = Modifier
+                                .padding(horizontal = 10.dp)
+                                .height(18.dp)
+                                .width(1.dp)
+                                .background(colorGreyText)
+                        )
+
+                        Text(
+                            text = "Box",
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.weight(1f)
+                        )
+                        Icon(
+                            Icons.Default.KeyboardArrowDown,
+                            contentDescription = "",
+                            tint = colorGreyText
+                        )
+                    }
+                }
+
+                Spacer(12)
+                SectionHeading("Categories", "What are you sending?")
+                Spacer(12)
+
+                FlowRow {
+                    categoryOption.forEach {
+                        AssistChip(
+                            colors = AssistChipDefaults.assistChipColors(
+                                containerColor = if (selectedCategory == it) Color.Black else Color.White
+                            ),
+                            onClick = { selectedCategory = it },
+                            label = {
+                                Text(
+                                    it,
+                                    color = if (selectedCategory == it) Color.White else Color.Black
+                                )
+                            },
+                            leadingIcon = {
+                                if (selectedCategory == it) Icon(
+                                    Icons.Default.Check,
+                                    contentDescription = "",
+                                    tint = Color.White,
+                                    modifier = Modifier.size(14.dp)
+                                )
+                            },
+                            modifier = Modifier.padding(horizontal = 4.dp)
+                        )
+                    }
+                }
+
             }
 
-            Spacer(12)
-            SectionHeading("Categories", "What are you sending?")
-            Spacer(12)
-
+            //BUTTON
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(containerColor = colorOrange)
+            ) {
+                Text("Calculate", modifier = Modifier.padding(vertical = 8.dp))
+            }
         }
     }
 }
